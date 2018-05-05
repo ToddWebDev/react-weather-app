00a125dcca384b328b7a1c5244c5b0ef09b451dd
ToddWebDev <webdevtodd@gmail.com>
2018-05-05 17:10:46 -0500

    try another api query to verify key success

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,11 +2,12 @@ var axios = require('axios');
 
 module.exports = {
   fetchWeather: function () {
-    var encodedURI = window.encodeURI(' http://api.openweathermap.org/data/2.5/weather?q=CITY-NAME&type=accurate&APPID=980f2ba197f48f7cc33e7e221e8e55a8');
+    var encodedURI = window.encodeURI('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=980f2ba197f48f7cc33e7e221e8e55a8');
 
     return axios.get(encodedURI)
       .then(function (response) {
-        return response.data.items;
+        return response.data;
+        console.log(response.data);
       });
   }
 };
