1026d834135a84d900888eb72bd33bdc47a84a63
Todd Rizzolo <todd.rizzolo@lifeway.com>
2018-05-15 15:25:46 -0500

    Consolidate WeatherForm in order to share state

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Menu from './Menu'
 import Home from './Home'
+import WeatherForm from './WeatherForm'
 import Forecast from './Forecast'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
@@ -22,7 +23,7 @@ class App extends React.Component {
           <Menu />
           <br/>
           <Switch>
-            <Route exact path ='/' component={Home} />
+            <Route exact path ='/' component={WeatherForm} />
             <Route path ='/forecast' component={Forecast} />
             <Route render={function() {
                 return <p>Not Found</p>
--- app/components/WeatherForm.js
+++ app/components/WeatherForm.js
@@ -3,6 +3,20 @@ import {getCurrentWeather} from '../utils/api'
 import PropTypes from 'prop-types'
 import {Redirect} from 'react-router-dom'
 
+class CurrentWeather extends React.Component {
+  render() {
+    return (
+      <div className="container">
+      <div className="row">
+        <div className="col s12">
+          <h1>Current Weather for Place</h1>
+        </div>
+      </div>
+      </div>
+    )
+  }
+}
+
 class WeatherForm extends React.Component {
   static propTypes = {
     zipcode: PropTypes.string,
@@ -28,7 +42,8 @@ class WeatherForm extends React.Component {
           console.log(weather);
           this.setState(function() {
             return {
-              toForecast: true,
+              toCurrentWeather: true,
+              toForecast: false,
               weather: weather
             }
           })
@@ -41,23 +56,30 @@ class WeatherForm extends React.Component {
 
     return (
       <div className="row jumbotron">
-        <h1>What's My Weather?</h1>
-        <br/>
-        <form className="col s12" onSubmit={this.handleSubmit}>
-          <div className="row">
-            <div className="input-field col m6 offset-m3 s12">
-              <i className="material-icons prefix">home</i>
-              <input id="icon_prefix" type="text" className="validate" value={this.state.query}
-          onChange={this.handleChange} required/>
-              <label htmlFor="icon_prefix">Enter City Name</label>
+        {!this.state.toCurrentWeather &&
+        <div>
+          <h1>What's My Weather?</h1>
+          <br/>
+          <form className="col s12" onSubmit={this.handleSubmit}>
+            <div className="row">
+              <div className="input-field col m6 offset-m3 s12">
+                <i className="material-icons prefix">home</i>
+                <input id="icon_prefix" type="text" className="validate" value={this.state.query}
+            onChange={this.handleChange} required/>
+                <label htmlFor="icon_prefix">Enter City Name</label>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col m6 offset-m3 s12">
-              <button type="submit" className="btn waves-effect waves-light btn-large btn-block">Get My Weather</button>
+            <div className="row">
+              <div className="col m6 offset-m3 s12">
+                <button type="submit" className="btn waves-effect waves-light btn-large btn-block">Get My Weather</button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
+        }
+        {this.state.toCurrentWeather &&
+          <CurrentWeather />
+        }
       </div>
     )
   }
