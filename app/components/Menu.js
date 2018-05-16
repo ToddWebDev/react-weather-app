7285250601d3319f4a926baae3d43c75b02fc60e
ToddWebDev <webdevtodd@gmail.com>
2018-05-15 19:45:13 -0500

    Fix Menu Button

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import api from '../utils/api'
 import WeatherForm from './WeatherForm'
+import {Link} from 'react-router-dom';
+
 
 
 class Menu extends React.Component {
@@ -13,7 +15,7 @@ class Menu extends React.Component {
         <div className="nav-wrapper">
           <a href="" className="brand-logo">React Weather App</a>
           <ul className="right hide-on-med-and-down">
-            <li><a href=""><i className="material-icons right">view_module</i>Link with Right Icon</a></li>
+            <li><a href=""><i className="material-icons right"></i>Get Weather</a></li>
           </ul>
         </div>
       </nav>
