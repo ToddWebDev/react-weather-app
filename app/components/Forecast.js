134d715dfa3c149d8e9816866b57ec6cab3b0987
ToddWebDev <webdevtodd@gmail.com>
2018-05-20 09:39:22 -0500

    Styles for ForecastDetails

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -5,13 +5,19 @@ import {Redirect, Link} from 'react-router-dom';
 
 function ForecastDetail(props) {
   return (
-    <div className="row">
+    <div className="row jumbotron">
       <div className="col s12">
         <h3>5 Day Forecast for <span className="uppercase">{props.weather.city.name}</span></h3>
-        <ul>
-          {props.weather.list.map(day => 
-            <li key={day.dt}>{day.weather[0].description}</li>
-          )}
+        <ul className="flex-box">
+          {props.weather.list.map((day) => (
+            <li className="flex-item" key={day.dt}>
+              <h5 className="uppercase">{day.weather[0].description}</h5>
+              <img className='weather small' src={'./app/images/weather-icons/' + day.weather[0].icon + '.svg'} alt='Weather' />
+              <br/>
+              <p className="red-text text-lighten-2">{day.main.temp.toFixed(0)}</p>
+              <h5 className="uppercase label red-text text-lighten-2">degrees</h5>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
@@ -48,14 +54,14 @@ class Forecast extends React.Component {
     }
     return (
       <div className="container">
-      <div className="row">
-        <div className="col s12">
-          <Link className='waves-effect waves-light btn' to='/'>Get More Weather</Link>
+        {this.state.loading 
+            ? <h3>Loading...</h3>
+            : <ForecastDetail weather={this.state.weather} />}
+        <div className="row jumbotron">
+          <div className="col s12">
+            <Link className='waves-effect waves-light btn' to='/'>Get More Weather</Link>
+          </div>
         </div>
-      </div>
-      {this.state.loading 
-          ? <h3>Loading...</h3>
-          : <ForecastDetail weather={this.state.weather} />}
       </div>
     )
   }
